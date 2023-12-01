ab360221845c1c1f869b45d1d539ccb1ba56f86a
alligatorboy <50695958+alligatorboy@users.noreply.github.com>
2023-12-01 15:59:08 +0900

    Add files via upload

---
+++ script/script01.js
@@ -0,0 +1,11 @@
+function menuOpen() {
+    document.querySelector('nav').style.display = "block";
+    document.querySelector('.xbtnbox').style.display = "block";
+    document.querySelector('.hambtnbox').style.display = "none";
+}
+
+function menuClose() {
+    document.querySelector('nav').style.display = "none";
+    document.querySelector('.xbtnbox').style.display = "none";
+    document.querySelector('.hambtnbox').style.display = "block";
+}
